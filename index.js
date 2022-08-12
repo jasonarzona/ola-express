bf5a8251517a5a5bebfb990dfef14b44dcf0497f
jzun <103344507+jasonarzona@users.noreply.github.com>
2022-08-12 06:06:50 +0000

    finishing touches

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Wassup Beep!')
 })
 
+app.get('/about', (req, res) => {
+  res.send('This is our about page!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
